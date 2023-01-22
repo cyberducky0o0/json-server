f0befce8312be18d307f88adf4b6a44aa5a2e64d
CyberDucky <juansoberanes21@gmail.com>
2023-01-22 12:24:03 -0800

    remving more

--- public/script.js
+++ public/script.js
@@ -7,43 +7,6 @@ function ResourceItem({ name, length }) {
   `
 }
 
-function ResourceList({ db }) {
-  return `
-    <ul>
-      ${Object.keys(db)
-        .map((name) =>
-          ResourceItem({
-            name,
-            length: Array.isArray(db[name]) && db[name].length,
-          })
-        )
-        .join('')}
-    </ul>
-  `
-}
-
-function NoResources() {
-  return `<p>No resources found</p>`
-}
-
-function ResourcesBlock({ db }) {
-  return `
-    <div>
-      <h1>Resources</h1>
-      ${Object.keys(db).length ? ResourceList({ db }) : NoResources()}
-    </div>
-  `
-}
-
-window
-  .fetch('db')
-  .then((response) => response.json())
-  .then(
-    (db) =>
-      (document.getElementById('resources').innerHTML = ResourcesBlock({ db }))
-  )
-
-function CustomRoutesBlock({ customRoutes }) {
   const rules = Object.keys(customRoutes)
   if (rules.length) {
     return `
